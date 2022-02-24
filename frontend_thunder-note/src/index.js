2264829720827164f64223513398dbc70b154f5a
rupeshgupta369 <rupeshgupta369@gmail.com>
2022-02-24 19:35:39 +0530

    implementing redux functionalities in app

--- frontend_thunder-note/src/index.js
+++ frontend_thunder-note/src/index.js
@@ -9,9 +9,7 @@ import store from "./store"
 
 ReactDOM.render(
   <Provider store={store}>
-    {/* <React.StrictMode> */}
     <App />
-    {/* </React.StrictMode> */}
   </Provider>,
   document.getElementById('root')
 );
--- frontend_thunder-note/src/screens/LoginScreen/LoginScreen.js
+++ frontend_thunder-note/src/screens/LoginScreen/LoginScreen.js
@@ -2,47 +2,67 @@ import React, { useState, useEffect } from 'react';
 import { Button, Col, Form, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import MainScreen from '../../components/MainScreen/MainScreen';
-import axios from 'axios';
+// import axios from 'axios';
 import "./LoginScreen.css"
 import Loading from '../../components/Loading';
 import ErrorMessage from '../../components/ErrorMessage';
+import { useDispatch, useSelector } from "react-redux"
+import { login } from '../../actions/userActions';
+
+
 
 const LoginScreen = ({ history }) => {
+
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [error, setError] = useState(false);
-    const [loading, setLoading] = useState(false);
 
+    const dispatch = useDispatch()
+
+    const userLogin = useSelector(state => state.userLogin)
+    const { loading, error, userInfo } = userLogin
+    // using redux here
+    // const [error, setError] = useState(false);
+    // const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        if (userInfo) {
+            history.push("/mynotes")
+        }
+    }, [history, userInfo]);
 
     const submitHandler = async (e) => {
         e.preventDefault();
+
+        dispatch(login(email, password))
         // console.log(email, password);
 
         /* 1. Try to create a variable called config.
         2. If you can't, then just move on.
         3. Otherwise, set the headers key of the config variable to an object with a key of
         "Content-type" and a value of "application / json". */
-        try {
-            const config = {
-                headers: {
-                    "Content-type": "application/json"
-                }
-            }
-            setLoading(true);
 
-            const { data } = await axios.post("/api/users/login", { email, password }, config)
-
-            console.log(data);
-
-            localStorage.setItem("userInfo", JSON.stringify(data));
-
-            setLoading(false)
-
-        } catch (error) {
-            setError(error.response.data.message)
-            setLoading(false)
-
-        }
+        // using redux instead of this for state mgmt
+        /*   try {
+              const config = {
+                  headers: {
+                      "Content-type": "application/json"
+                  }
+              }
+              setLoading(true);
+  
+              const { data } = await axios.post(
+                  "/api/users/login",
+                  { email, password },
+                  config
+              )
+              console.log(data);
+              localStorage.setItem("userInfo", JSON.stringify(data));
+              setLoading(false)
+  
+          } catch (error) {
+              setError(error.response.data.message)
+              setLoading(false)
+          } */
     }
 
     return (
@@ -79,7 +99,7 @@ const LoginScreen = ({ history }) => {
                 </Row>
 
             </div>
-        </MainScreen >
+        </MainScreen>
     )
 };
 
